5642b814d0ce961be5ebcdcc09e052ef1a62e8d7
Avesh <aveshhasanfatta1155@gmail.com>
2024-09-22 14:31:36 +0530

    fix : added vars in the env

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -4,7 +4,10 @@ module.exports = {
   PORT: process.env.PORT,
   DB_URI: process.env.DB_URI,
   VERIFY_TOKEN_SECRET: process.env.VERIFY_TOKEN_SECRET,
-  LIVE_URL: process.env.LIVE_URL,
+  AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
   MAIL_SERVICE_URL: process.env.MAIL_SERVICE_URL,
+  CHAT_SERVICE_URL: process.env.CHAT_SERVICE_URL,
+  POST_SERVICE_URL: process.env.POST_SERVICE_URL,
+  FRONTEND_URL: process.env.FRONTEND_URL,
 };
